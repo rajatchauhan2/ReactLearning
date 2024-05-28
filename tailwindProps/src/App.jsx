baef1e07ff1d08b5b08663fe25f17eb8d53abdd7
rajatchauhan1 <rajat.tca1901089@tmu.ac.in>
2024-05-28 19:00:38 +0530

    Project

---
+++ tailwindProps/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './components/Card'
+function App() {
+
+  return (
+    <>
+  <h1 className='bg-green-400 text-black p-5 rounded-xl mb-4'>hello Tailwinds</h1>
+<Card username="rajat" btnText="Visit me"/>
+<Card username="hitesh" btnText=" Click Me"/>
+
+
+  </>
+  )
+}
+
+export default App
--- vite-project/src/chai.jsx
+++
@@ -1,6 +0,0 @@
-function Chai (){
-    return(
-        <h2>hello chai</h2>
-    )
-}
-export default Chai
