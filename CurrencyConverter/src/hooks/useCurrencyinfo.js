d7a4adfdbccff62186a7bbaaabe290fb15e0ba69
rajatchauhan1 <rajat.tca1901089@tmu.ac.in>
2024-05-31 18:44:50 +0530

    SpellingErrors improveLogic

--- CurrencyConverter/src/hooks/useCurrencyinfo.js
+++ CurrencyConverter/src/hooks/useCurrencyinfo.js
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react"
 
-function useCurruncyinfo(currency) {
+
+function useCurrencyInfo(currency){
     const [data, setData] = useState({})
-    useEffect(() =>{
+    useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then(() => resizeBy.josn())
-        .then(() => setData(res[currency]))
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
         console.log(data);
     }, [currency])
     console.log(data);
-    return data;
-    
+    return data
 }
-export default useCurruncyinfo;
+
+export default useCurrencyInfo;
